6431cdca754a024f60ad3e58ee76c32d2a906998
GiovanaZanirato <84473403+GiovanaZanirato@users.noreply.github.com>
2021-06-09 13:58:38 -0300

    Add files via upload

---
+++ Giovana_210608.py
@@ -0,0 +1,69 @@
+#question 2
+fruit=["banana","kiwi","orange","pear","limon"]
+#question 3
+print(fruit)
+#question 4
+fruit.sort()
+#question 5
+print(fruit)
+#question 6
+fruit.append("apple")
+#question 7 
+print(fruit)
+#question 8
+fruit.insert(0,"zucchini")
+#question 9
+print(fruit)
+#question 10
+oculos=(sorted(fruit))
+#question 11
+print(fruit)
+print(oculos)
+#question 12
+fruit.append("tomato")
+#question 13
+print(fruit)
+print(oculos)
+#question 14
+list=["happier","belive","summer in the city"]
+#question 15
+print(list)
+#question 16
+for music in list:
+    print(music)
+#question 17
+print("**************************")
+for music in list:
+    print(music)
+    print(music)
+#quetion 18
+print("***********")
+for music in list:
+    print(music.upper())
+#question 19
+print("***************")
+print("this is my list")
+for music in list:
+    print(music)
+#question 20
+print("******************")
+for music in list:
+    print(music)
+print("This was my list")
+#question 21
+print("*********************")
+print("this is one item of my list")
+print(list[0])
+print("This is one item of my list")
+print(list[1])
+print("This is one item of my list")
+print(list[2])
+#question 22
+print("***********************************")
+print(list[0])
+print("This was one item of my list")
+print(list[1])
+print("This was one item of my list")
+print(list[2])
+print("This was one item of my list")
+
